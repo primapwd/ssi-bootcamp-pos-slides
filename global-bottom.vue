94a544dc790a7adb49e66901ca2e3497091e6018
Prima Putra <primapwd@gmail.com>
2024-04-23 22:55:12 +0700

    feat: added global bottom footer component

---
+++ global-bottom.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="absolute bottom-0 left-0 right-0 p-2 text-center text-xs font-sans text-gray-500/50">
+        Bangun Aplikasi Kasirmu | &copy; 2024 - present by SSI Academy Mini Bootcamp
+    </footer>
+</template>
